5c2a028d0a95020e4a09c241629853fb0f355487
p6352138 <coolpwh@qq.com>
2018-11-01 15:10:32 +0800

    初步调试

--- lanuch.py
+++ lanuch.py
@@ -1,8 +1,12 @@
 from urllib.request import urlopen
+from bs4 import BeautifulSoup
 
 html = urlopen('https://www.taptap.com/top/download')
 
 bs_obj = BeautifulSoup(html.read(),'html.parser')
+text_list = bs_obj.find_all("div","taptap-top-card")
 
-print(html.read())
+for text in text_list:
+    print(text.get_text())
+
 html.close()
